c24f5b6b71d6d4666fe0526e032a650fa9ad5653
Adam Noir <webnoircode@gmail.com>
2025-04-24 19:32:11 -0600

    changes in prisma client

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -154,11 +154,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "POSTGRES_URL",
-        "value": "postgresql://postgres:123456@localhost:5432/TODO"
+        "value": null
       }
     }
   },
